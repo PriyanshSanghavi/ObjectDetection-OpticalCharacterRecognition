73ff815d2b9071683acb1a13a180cc932dfae233
Priyansh Sanghavi <priyansh4299@gmail.com>
2022-09-19 21:58:44 +0530

    Add files via upload

---
+++ object_detection_video.py
@@ -0,0 +1,46 @@
+import cv2
+import numpy as np
+cam = cv2.VideoCapture(0)
+# video dimensions
+
+#length
+# cam.set(3,740)
+# width
+# cam.set(4,580)
+
+
+classNames=[]
+classFile='coco.objects'
+
+with open(classFile,'rt') as f:
+    classNames=f.read().rstrip('\n').split('\n')
+
+
+configPath='coco.pbtxt'
+weightPath='frozen_inference_graph.pb'
+
+net = cv2.dnn_DetectionModel(weightPath, configPath)
+net.setInputSize(320 , 230)
+net.setInputScale(1.0 / 127.5)
+net.setInputMean((127.5, 127.5, 127.5))
+net.setInputSwapRB(True)
+# clssIds: the returned ids , confs: the confidence , bbox: the drawn box
+while True:
+    ret, img = cam.read()
+   
+    classIds,confs,bbox=net.detect(img,confThreshold=0.5)
+    #confThreshold
+    print(classIds,bbox)
+    if len(classIds) !=0:
+        for classid, confidence, box in zip(classIds.flatten() ,confs.flatten(), bbox):
+            cv2.rectangle(img , box ,color=(0,255,0),thickness=1)
+            cv2.putText(img , classNames[classid-1] , (box[0]+10 ,box[1]+20 ),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),thickness=1)
+
+    cv2.imshow('frame',img)
+    if cv2.waitKey(0) & 0xFF == 27:
+        break
+    
+   
+
+cam.release()
+cv2.destroyAllWindows()
